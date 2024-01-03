dcf2b69845e0e5198c9f29e0f41f298d5986674a
arpan-kumar-saini <arpansaini55@gmail.com>
2024-01-03 21:50:07 +0530

    third commit

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -16,14 +16,14 @@ function Login() {
                 
                 <div className="relative mb-6" data-te-input-wrapper-init>
                   <input
-                    type="text"
+                    type="number"
                     className="peer block min-h-[auto] w-full border-2 rounded  bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                     id="exampleFormControlInput3"
-                    placeholder="Email address" />
+                    placeholder="Phone Number" />
                   <label
                     
                     className="pointer-events-none  absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-                    >Email address
+                    >Phone Number
                   </label>
                 </div>         
                 
@@ -62,24 +62,7 @@ function Login() {
                   
                   Continue with Google
                 </a>
-                <a
-                  className="mb-3 flex w-full items-center justify-center bg-[#55acee] rounded bg-info px-7 pb-2.5 pt-3 text-center text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#54b4d3] transition duration-150 ease-in-out hover:bg-info-600 hover:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] focus:bg-info-600 focus:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] focus:outline-none focus:ring-0 active:bg-info-700 active:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.3),0_4px_18px_0_rgba(84,180,211,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(84,180,211,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(84,180,211,0.2),0_4px_18px_0_rgba(84,180,211,0.1)]"
-                  
-                  href="#!"
-                  role="button"
-                  data-te-ripple-init
-                  data-te-ripple-color="light">
-                  
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="mr-2 h-3.5 w-3.5"
-                    fill="currentColor"
-                    viewBox="0 0 24 24">
-                    <path
-                      d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
-                  </svg>
-                  Continue with Facebook
-                </a>
+                
               </form>
             </div>
           </div>
