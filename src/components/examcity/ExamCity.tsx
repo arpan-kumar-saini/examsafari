d1f9051b5007a62473f029668f3ef9dcb76eaac2
Arpan <arpansaini55@gmail.com>
2024-02-21 14:19:42 +0530

    Update ExamCity.tsx

--- src/components/examcity/ExamCity.tsx
+++ src/components/examcity/ExamCity.tsx
@@ -8,7 +8,7 @@ function ExamCity() {
         <div className='md:flex gap-4 md:mx-28 px-8'>
             <Link to="/allahabad">
             <div className="relative">
-                <img className=" h-[300px] w-auto rounded-[25px] p-2  " src="./public/images/allahabad.png" alt="dummy-image"/>
+                <img className=" h-[300px] w-auto rounded-[25px] p-2  " src="https://github.com/arpan-kumar-saini/examsafari/blob/main/public/images/allahabad1.jpeg" alt="dummy-image"/>
                 <a href="" className='absolute top-1 ml-2 text-2xl font-semibold text-neutral-50 pt-6 pl-4'></a>
                 <button className="absolute bottom-2 left-3 bg-red-500 text-white p-2 rounded-[15px] hover:bg-blue-600 m-2">Allahabad..</button>
             </div>
@@ -16,7 +16,7 @@ function ExamCity() {
 
             <Link to="/lucknow">
             <div className="relative">
-                <img className=" h-[300px] w-auto rounded-[25px] p-2" src="./public/images/lucknow.png" alt="dummy-image"/>
+                <img className=" h-[300px] w-auto rounded-[25px] p-2" src="https://github.com/arpan-kumar-saini/examsafari/blob/main/public/images/lucknow1.jpeg" alt="dummy-image"/>
                 <a href="" className='absolute top-1 ml-2 text-2xl font-semibold uppercase text-neutral-50 pt-6 pl-4'></a>
             <button className="absolute bottom-2 left-3 bg-red-500 text-white p-2 rounded-[15px] hover:bg-blue-600 m-2">Lucknow..</button>
             </div>
@@ -24,7 +24,7 @@ function ExamCity() {
 
             <Link to="/kanpur">
             <div className="relative">
-                <img className="h-[300px] w-auto rounded-[25px] p-2" src="./public/images/kanpur.png" alt="dummy-image"/>
+                <img className="h-[300px] w-auto rounded-[25px] p-2" src="https://github.com/arpan-kumar-saini/examsafari/blob/main/public/images/kanpur.jpeg" alt="dummy-image"/>
                 <a href="" className='absolute top-1 ml-2 text-2xl font-semibold text-neutral-50 pt-6 pl-4'></a>
                 <button className="absolute bottom-2 left-3 bg-red-500 text-white p-2 rounded-[15px] hover:bg-blue-600 m-2">Kanpur..</button>
             </div>
