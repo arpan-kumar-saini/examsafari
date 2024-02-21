cbba2a9f1dd7952b9541785f91c0fc8618134385
Arpan <arpansaini55@gmail.com>
2024-02-21 14:12:50 +0530

    Update Services.tsx

--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -7,19 +7,19 @@ function Services() {
         <div className='text-sm md:text-xl text-[#6B7280]  ml-28 md:ml-[170px] md:mt-2'>Little text about services </div>
         <div className='md:flex gap-4 md:mx-32 px-8'>
         <div className="relative">
-            <img className=" h-[300px] w-auto rounded-[25px] p-2 " src="./public/images/service1.png" alt="dummy-image"/>
+            <img className=" h-[300px] w-auto rounded-[25px] p-2 " src="https://github.com/arpan-kumar-saini/examsafari/blob/main/public/images/service1.png" alt="dummy-image"/>
             <a href="" className='absolute top-1 ml-2 text-2xl font-semibold text-neutral-50 pt-6 pl-4'></a>
             <button className="absolute bottom-2 left-3 bg-red-500 text-white p-2 rounded-[15px] hover:bg-blue-600 m-2">View More..</button>
         </div>
 
         <div className="relative">
-            <img className=" h-[300px] w-auto rounded-[25px] p-2" src="./public/images/service2.png" alt="dummy-image"/>
+            <img className=" h-[300px] w-auto rounded-[25px] p-2" src="https://github.com/arpan-kumar-saini/examsafari/blob/main/public/images/service2.png" alt="dummy-image"/>
             <a href="" className='absolute top-1 ml-2 text-2xl font-semibold uppercase text-neutral-50 pt-6 pl-4'></a>
          <button className="absolute bottom-2 left-3 bg-red-500 text-white p-2 rounded-[15px] hover:bg-blue-600 m-2">View More..</button>
         </div>
 
         <div className="relative">
-            <img className="h-[300px] w-auto rounded-[25px] p-2" src="./public/images/service3.png" alt="dummy-image"/>
+            <img className="h-[300px] w-auto rounded-[25px] p-2" src="https://github.com/arpan-kumar-saini/examsafari/blob/main/public/images/service3.png" alt="dummy-image"/>
             <a href="" className='absolute top-1 ml-2 text-2xl font-semibold text-neutral-50 pt-6 pl-4'></a>
             <button className="absolute bottom-2 left-3 bg-red-500 text-white p-2 rounded-[15px] hover:bg-blue-600 m-2">View More..</button>
         </div>
